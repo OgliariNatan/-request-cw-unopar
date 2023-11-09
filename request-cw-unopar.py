693d07482926437a768fc3ec559fca24c46569bd
OgliariNatan <natanogliari@gmail.com>
2023-11-09 10:33:16 -0300

    teste de mesclagem do gitrthf

--- request-cw-unopar.py
+++ request-cw-unopar.py
@@ -19,4 +19,3 @@ navegador = webdriver.Chrome(service=servico)
 unopar_cw = requests.get('https://www.colaboraread.com.br/integracaoAlgetec/index?usuarioEmail=EADPR29%40GMAIL.COM&usuarioNome=NATAN+OGLIARI&disciplinaDescricao=&atividadeId=3501230&atividadeDescricao=CW2+-+PROCESSO+DA+CRIATIVIDADE&ofertaDisciplinaId=11474268&codigoMaterial=0&fornecedor=2&urlOrigem=http%3A%2F%2Fcm-kls-content.s3.amazonaws.com%2F201601%2FINTERATIVAS_2_0%2FPROCESSO_DA_CRIATIVIDADE%2FU2%2FS1%2Findex.html&isAluno=true')
 
 print(unopar_cw)
-git p
