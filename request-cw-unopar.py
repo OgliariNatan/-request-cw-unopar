5f4ba1c1436aeeab91546283343fb66f1d35810e
OgliariNatan <natanogliari@gmail.com>
2024-10-05 17:45:40 -0300

    refotmatado

--- request-cw-unopar.py
+++ request-cw-unopar.py
@@ -17,36 +17,47 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-#servico = Service(ChromeDriverManager().install())
-#navegador = webdriver.Chrome(service=servico)
+# servico = Service(ChromeDriverManager().install())
+# navegador = webdriver.Chrome(service=servico)
 
-unopar_cw = requests.get('https://www.colaboraread.com.br/integracaoAlgetec/index?usuarioEmail=EADPR29%40GMAIL.COM&usuarioNome=NATAN+OGLIARI&disciplinaDescricao=&atividadeId=3501230&atividadeDescricao=CW2+-+PROCESSO+DA+CRIATIVIDADE&ofertaDisciplinaId=11474268&codigoMaterial=0&fornecedor=2&urlOrigem=http%3A%2F%2Fcm-kls-content.s3.amazonaws.com%2F201601%2FINTERATIVAS_2_0%2FPROCESSO_DA_CRIATIVIDADE%2FU2%2FS1%2Findex.html&isAluno=true')
+unopar_cw = requests.get(
+    "https://www.colaboraread.com.br/integracaoAlgetec/index?usuarioEmail=EADPR29%40GMAIL.COM&usuarioNome=NATAN+OGLIARI&disciplinaDescricao=&atividadeId=3501230&atividadeDescricao=CW2+-+PROCESSO+DA+CRIATIVIDADE&ofertaDisciplinaId=11474268&codigoMaterial=0&fornecedor=2&urlOrigem=http%3A%2F%2Fcm-kls-content.s3.amazonaws.com%2F201601%2FINTERATIVAS_2_0%2FPROCESSO_DA_CRIATIVIDADE%2FU2%2FS1%2Findex.html&isAluno=true"
+)
 
-print(unopar_cw)
+# print(unopar_cw)
 
-#https://www.hashtagtreinamentos.com/automacao-web-no-python
+# https://www.hashtagtreinamentos.com/automacao-web-no-python
 
 
-#navegador = webdriver.Firefox()
+# navegador = webdriver.Firefox()
 navegador = webdriver.Chrome()
 
-#navegador = webdriver.Chrome("chromedriver")
-navegador.get('https://www.colaboraread.com.br/login/auth')
-username = 'xxxxx'
-password = 'xxxxxx'
-#Espera carregar o site
+# navegador = webdriver.Chrome("chromedriver")
+navegador.get("https://www.colaboraread.com.br/login/auth")
+username = "user"
+password = "xxxx"
+# Espera carregar o site
 navegador.implicitly_wait(20)
-navegador.find_element('id', 'username').send_keys(username)
+navegador.find_element("id", "username").send_keys(username)
 navegador.implicitly_wait(10)
-navegador.find_element('id', 'password').send_keys(password)
+navegador.find_element("id", "password").send_keys(password)
 navegador.implicitly_wait(10)
-#clica no botão de entrar
-al = WebDriverWait(navegador, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/button')))
+# clica no botão de entrar
+al = WebDriverWait(navegador, 30).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/button'))
+)
 al.click()
-#Entra na trilha e carreira
-al = WebDriverWait(navegador, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="navbar-content-aluno-cursos"]/div/div[2]/div/div[2]/div[2]/button')))
+# Entra na trilha e carreira
+al = WebDriverWait(navegador, 30).until(
+    EC.presence_of_element_located(
+        (
+            By.XPATH,
+            '//*[@id="navbar-content-aluno-cursos"]/div/div[1]/div/div[3]/form/div[2]/button',
+        )
+    )
+)
 al.click()
-
+input("Clique para seguir")
 
 navegador.quit()
 navegador.close()
--- sub-request.py
+++ sub-request.py
@@ -15,20 +15,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-#servico = Service(ChromeDriverManager().install())
-#navegador = webdriver.Chrome(service=servico)
+# servico = Service(ChromeDriverManager().install())
+# navegador = webdriver.Chrome(service=servico)
 unopar_cw = webdriver.Chrome()
-url='https://www.colaboraread.com.br/integracaoAlgetec/index?usuarioEmail=EADPR29%40GMAIL.COM&usuarioNome=NATAN+OGLIARI&disciplinaDescricao=&atividadeId=3684159&atividadeDescricao=CW1+-+TECNOLOGIAS+DE+INFORMA%C3%87%C3%83O+APLICADAS+AO+DIREITO&ofertaDisciplinaId=12293439&codigoMaterial=0&fornecedor=2&urlOrigem=http%3A%2F%2Fcm-kls-content.s3.amazonaws.com%2F201802%2FINTERATIVAS_2_0%2FTECNOLOGIAS_DE_INFORMACAO_APLICADAS_AO_DIREITO%2FU1%2FS1%2Findex.html&isAluno=true'
+url = "https://www.colaboraread.com.br/integracaoAlgetec/index?usuarioEmail=EADPR29%40GMAIL.COM&usuarioNome=NATAN+OGLIARI&disciplinaDescricao=&atividadeId=3684159&atividadeDescricao=CW1+-+TECNOLOGIAS+DE+INFORMA%C3%87%C3%83O+APLICADAS+AO+DIREITO&ofertaDisciplinaId=12293439&codigoMaterial=0&fornecedor=2&urlOrigem=http%3A%2F%2Fcm-kls-content.s3.amazonaws.com%2F201802%2FINTERATIVAS_2_0%2FTECNOLOGIAS_DE_INFORMACAO_APLICADAS_AO_DIREITO%2FU1%2FS1%2Findex.html&isAluno=true"
 unopar_cw.get(url)
 unopar_cw = requests.get(url)
 unopar_cw.implicitly_wait(20)
 print(unopar_cw)
 
 
-#clica no botão de entrar
-al = WebDriverWait(unopar_cw, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="navegacao"]/a')))
+# clica no botão de entrar
+al = WebDriverWait(unopar_cw, 30).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="navegacao"]/a'))
+)
 al.click()
 
 
 unopar_cw.quit()
-#unopar_cw.close()
+# unopar_cw.close()
